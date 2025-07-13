21e172c8ee254283b6a69c1c43f2959e62d8f716
mango-magic <isaac@manymangoes.com>
2025-07-14 07:51:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 APP = ['workflow_tracker.py']  # Use the filename of your main script
 DATA_FILES = []
 OPTIONS = {
-    'argv_emulation': True,
+    'argv_emulation': False,
     'packages': ['rumps', 'pynput', 'requests'],
     'includes': ['AppKit', 'Foundation', 'Quartz'],  # Added Foundation if needed
     'iconfile': 'Magic Clone.png',  # Path to the PNG icon in the repo root; py2app converts to .icns
