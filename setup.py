7b7e1029c939b01e1b0e003f4ddf5de1934df157
mango-magic <isaac@manymangoes.com>
2025-07-14 10:24:47 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
 from setuptools import setup
-import os
-import cv2
-import glob
-
-# --- Find and prepare OpenCV data files ---
-cv2_data_path = os.path.join(os.path.dirname(cv2.__file__), 'data')
-cv2_data_files = glob.glob(os.path.join(cv2_data_path, '*.xml'))
 
 # --- Application Details ---
 APP = ['workflow_tracker.py']
-DATA_FILES = [
-    'Magic Clone.png', 'icon_active.png', 'icon_inactive.png',
-    ('data', cv2_data_files) # Include the OpenCV data files
-]
+DATA_FILES = ['Magic Clone.png', 'icon_active.png', 'icon_inactive.png']
 
 # --- py2app Options ---
+# This section is updated to match the application's actual needs.
+# It removes outdated packages (cv2, numpy, pytesseract) and adds the 'Vision' framework.
 OPTIONS = {
     'argv_emulation': False,
-    # Add cv2 and numpy to the packages list
-    'packages': ['rumps', 'pynput', 'requests', 'mss', 'pytesseract', 'PIL', 'cv2', 'numpy'],
-    'includes': ['AppKit', 'Foundation', 'Quartz', 'imp'],
+    'packages': ['rumps', 'pynput', 'requests', 'mss', 'PIL'],
+    'includes': ['AppKit', 'Foundation', 'Quartz', 'Vision', 'imp'],
     'iconfile': 'Magic Clone.png',
     'plist': {
         'CFBundleDisplayName': 'Mango Clone',
