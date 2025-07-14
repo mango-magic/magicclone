085d367d47ed83691a8fc847b8f63518e4614b74
mango-magic <isaac@manymangoes.com>
2025-07-14 10:29:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ DATA_FILES = ['Magic Clone.png', 'icon_active.png', 'icon_inactive.png']
 OPTIONS = {
     'argv_emulation': False,
     'packages': ['rumps', 'pynput', 'requests', 'mss', 'PIL'],
-    # 'CoreGraphics' is now explicitly included to resolve the launch error.
-    'includes': ['AppKit', 'Foundation', 'Quartz', 'Vision', 'CoreGraphics', 'imp'],
+    'includes': ['AppKit', 'Foundation', 'Quartz', 'Vision', 'CoreGraphics', 'imp'], # <--- CORRECTED LINE
     'iconfile': 'Magic Clone.png',
     'plist': {
         'CFBundleDisplayName': 'Mango Clone',
